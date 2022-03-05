ab1657395a56d3ae5403b9daab1a0ab09bd5027a
Tiago Angelo <kurtis.angelo@gmail.com>
2022-03-05 14:52:34 -0300

    Generate ConfigMaps from GitRepositories

--- pkg/handler/configmap_creation.go
+++ pkg/handler/configmap_creation.go
@@ -3,27 +3,28 @@ package handler
 import (
 	"context"
 
-	mf "github.com/manifestival/manifestival"
-
 	"github.com/angelokurtis/reconciler"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/angelokurtis/configmap-generator/api/v1beta1"
 	"github.com/angelokurtis/configmap-generator/internal/kustomize"
+	"github.com/angelokurtis/configmap-generator/internal/manifest"
+	"github.com/angelokurtis/configmap-generator/pkg/transformer"
 )
 
 type ConfigMapCreation struct {
 	reconciler.Funcs
-	kustomize Kustomize
+	kustomize    Kustomize
+	transformers *transformer.Builder
 }
 
-func NewConfigMapCreation(kustomize Kustomize) *ConfigMapCreation {
-	return &ConfigMapCreation{kustomize: kustomize}
+func NewConfigMapCreation(kustomize Kustomize, transformers *transformer.Builder) *ConfigMapCreation {
+	return &ConfigMapCreation{kustomize: kustomize, transformers: transformers}
 }
 
 type Kustomize interface {
-	GenerateConfigMap(ctx context.Context, source string, generator *kustomize.ConfigMapGenerator) (mf.Manifest, error)
+	Build(ctx context.Context, source string, kustomization *kustomize.Kustomization) (manifest.List, error)
 }
 
 func (c *ConfigMapCreation) Reconcile(ctx context.Context, obj client.Object) (ctrl.Result, error) {
@@ -38,12 +39,35 @@ func (c *ConfigMapCreation) Reconcile(ctx context.Context, obj client.Object) (c
 	return c.reconcile(ctx, source, gen)
 }
 
-func (c *ConfigMapCreation) reconcile(ctx context.Context, source string, gen *v1beta1.ConfigMapGenerator) (ctrl.Result, error) {
-	generator := &kustomize.ConfigMapGenerator{Name: gen.GetName(), Files: gen.Spec.Files, Literals: gen.Spec.Literals}
-	manifest, err := c.kustomize.GenerateConfigMap(ctx, source, generator)
+func (c *ConfigMapCreation) reconcile(ctx context.Context, source string, resource *v1beta1.ConfigMapGenerator) (ctrl.Result, error) {
+	mf, err := c.kustomize.Build(ctx, source, kustomizationFor(resource))
 	if err != nil {
 		return c.RequeueOnErr(ctx, err)
 	}
-	_ = manifest
-	return c.Next(ctx, gen)
+
+	transform := c.transformers.ForConfigMapGenerator(resource)
+	mf, err = mf.Transform(transform.Metadata)
+	if err != nil {
+		return c.RequeueOnErr(ctx, err)
+	}
+
+	if err = mf.Apply(ctx); err != nil {
+		return c.RequeueOnErr(ctx, err)
+	}
+	return c.Next(ctx, resource)
+}
+
+func kustomizationFor(resource *v1beta1.ConfigMapGenerator) *kustomize.Kustomization {
+	return &kustomize.Kustomization{
+		GeneratorOptions: &kustomize.GeneratorOptions{
+			DisableNameSuffixHash: true,
+			Labels:                resource.Spec.Labels,
+			Annotations:           resource.Spec.Annotations,
+		},
+		ConfigMapGenerator: []*kustomize.ConfigMapGenerator{{
+			Name:     resource.GetName(),
+			Files:    resource.Spec.Files,
+			Literals: resource.Spec.Literals,
+		}},
+	}
 }
